e0d6b5b7483dd11555226b0bb9f3af17c6ff4471
Eric Davenport <eric_dav@Erics-MacBook-Pro.local>
2019-09-24 15:51:57 -0400

    added xcode project

---
+++ Swift UI Demo/Swift UI Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift UI Demo
+//
+//  Created by Eric Davenport on 9/24/19.
+//  Copyright © 2019 Eric Davenport. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome iOS 6.3")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
